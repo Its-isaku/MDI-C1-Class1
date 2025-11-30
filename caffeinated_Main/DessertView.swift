54c12289656d458c5b98245c71654eca4b85929d
Its-Isaku <Isai.almeraz@uabc.edu.mx>
2025-11-30 13:22:45 -0800

    102-Assignment#2

---
+++ caffeinated_Main/DessertView.swift
@@ -0,0 +1,54 @@
+//
+//  DessertView.swift
+//  Test_Store
+//
+//  Created by Isai Magdaleno Almeraz Landeros on 29/11/25.
+//
+
+import SwiftUI
+
+struct DessertView: View {
+    // variables
+    let DessertItems = [
+        "Chocolate Brownie": 4.99,
+        "Tiramisu": 6.49,
+        "Cheesecake": 5.99,
+        "Carrot Cake": 5.49,
+        "Croissant": 3.99,
+        "Cinnamon Roll": 4.49,
+        "Blueberry Muffin": 3.49,
+        "Apple Pie": 4.99,
+        "Macarons": 6.99,
+        "Chocolate Chip Cookie": 2.99
+    ]
+    
+    
+    var SortedMenu: [(name:String, price:Double)] {
+        DessertItems
+            .sorted {$0.value > $1.value } // array of key,value
+            .map {(name: $0.key, price: $0.value)} // rename key:value to name:price
+    }
+    
+    var body: some View {
+        
+        List {
+            ForEach(SortedMenu, id: \.name) { Item in
+                HStack {
+                    Text(Item.name)
+                        .font(.headline)
+                    Spacer()
+                    Text(String(format: "$%.2f", Item.price))
+                        .foregroundStyle(.secondary)
+                }
+                .padding(.vertical,10)
+            }
+        }
+        
+        
+        
+    }
+}
+
+#Preview {
+    DessertView()
+}
--- caffeinated_Main/MenuView.swift
+++ caffeinated_Main/MenuView.swift
@@ -20,47 +20,118 @@ struct MenuView: View {
         "Hot Chocolate": 4.49
     ]
     
+    @State private var showMessage = false
+    @State private var showAffordable = false
+    @State private var showDesserts = false
+    
     
     // functions
     func getHighestPrice() -> String {
-        let maxValue = menuItems.max(by: { $0.value < $1.value })!.value
+        var maxValue = 0.0
+        for item in displayMenu {
+            if item.price > maxValue {
+                maxValue = item.price
+            }
+        }
         return String(format: "$%.2f", maxValue)
     }
     
     func getLowestPrice() -> String {
-        let maxValue = menuItems.min(by: { $0.value < $1.value })!.value
-        return String(format: "$%.2f", maxValue)
+        var minValue = displayMenu.first?.price ?? 0.0
+        for item in displayMenu {
+            if item.price < minValue {
+                minValue = item.price
+            }
+        }
+        return String(format: "$%.2f", minValue)
     }
     
+    func getTotalItems() -> Int {
+        displayMenu.count
+    }
+    
+    // computed properties
+    // ----Sytax----
+    /*
+     var propertyName: type {
+        return calculated value
+     }
+     */
+    var SortedMenu: [(name:String, price:Double)] {
+        menuItems
+            .sorted {$0.value > $1.value } // array of key,value
+            .map {(name: $0.key, price: $0.value)} // rename key:value to name:price
+    }
+    
+    var displayMenu: [(name:String, price:Double)] {
+        showAffordable ? SortedMenu.filter {$0.price < 4.0} : SortedMenu
+    }
+    
+    var avaragePrice : Double {
+        let prices = menuItems.values
+        let total = prices.reduce(0, +)
+        return total / Double(prices.count)
+    }
+    
+    // Main
     var body: some View {
         VStack{
             VStack{
-                Image("littleLemonLogo")
+                Image("H-FullLogo")
                     .resizable()
                     .scaledToFit()
-                    .frame(height:50)
+                    .frame(height:60)
                 
                 Text("Today's menu")
                     .font(.title2)
                     .bold()
             }
+            
+            
+            
+            // Toggles
+            VStack(){
+                Toggle("Show welcome Message", isOn: $showMessage)
+                Toggle("Show only affordable items < than $4.0", isOn: $showAffordable)
+                Button("show Desserts") {
+                    showDesserts = true
+                    print(showDesserts)
+                }
+                .foregroundColor(.white)
+                .font(.headline)
+                .padding()
+                .background(.brown.opacity(0.5))
+                .cornerRadius(10)
+                .sheet(isPresented: $showDesserts) {
+                    DessertView()
+                }
+            }
+            .padding()
+            
+            if showMessage {
+                Text("Welcome to Caffeinated!")
+                    .font(.headline)
+                    .foregroundColor(.brown)
+            }
         
             List {
-                ForEach(menuItems.sorted(by: { $0.value > $1.value }), id: \.key) { item, price in
+                ForEach(displayMenu, id: \.name) { Item in
                     HStack {
-                        Text(item)
+                        Text(Item.name)
                             .font(.headline)
                         Spacer()
-                        Text(String(format: "$%.2f", price))
+                        Text(String(format: "$%.2f", Item.price))
                             .foregroundStyle(.secondary)
                     }
                     .padding(.vertical,10)
                 }
             }
             
+            
+            
             Section{
                 VStack{
-                    Text("Total Items: \(menuItems.count)")
+                    Text("Total Items: \(getTotalItems())")
                     Text("Highest price: \(getHighestPrice())")
                     Text("Lowest price: \(getLowestPrice())")
                 }
